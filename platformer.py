a77344fac79c1617f09fa1d8d4ab32e27c379c45
theNaukiMatic <naukeshgoyal@gmail.com>
2020-12-21 17:49:45 +0530

    basic game window setup

--- platformer.py
+++ platformer.py
@@ -1,12 +1,30 @@
 import pygame
 
+#variables
+SCREEN_SIZE = (800,600)
+BACKROUND_COLOR = (121, 217, 215)
+
 #init
 pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("Working Title")
 
+
+isGameRunning = True
 #gameloop
+while(isGameRunning):
     #input
+    for event in pygame.event.get():
+
+        #quit button 
+        if(event.type == pygame.QUIT):
+            isGameRunning = False
+
     #updating
+
     #drawing
+    screen.fill(BACKROUND_COLOR)
+    pygame.display.flip()
 
 #quit
 pygame.quit()
